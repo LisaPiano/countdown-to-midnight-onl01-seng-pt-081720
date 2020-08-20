8dcd02824ae97f794411e564d99fdfd6bec94c3f
Lisa Walters <campi_l@fortlewis.edu>
2020-08-20 23:02:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
 
 "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{number} SECOND(S)!"
+    sleep(5)
+    number -= 1
+  end
+
+"HAPPY NEW YEAR!"
+end
